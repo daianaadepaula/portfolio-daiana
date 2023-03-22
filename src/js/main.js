e4d2a71f133f20f31d80cf0c9bbd27e0f8df3c89
Daiana de Paula <daianaadepaula1@gmail.com>
2023-03-22 09:13:19 -0300

    project section

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,7 @@ menuLinks.forEach((menuLink) => {
 //change background header
 function scrollHeader(){
 	const header = document.getElementById('header');
-	this.scrolly >= 20 ? header.classList.add('active') : header.classList.remove('active');
+	this.scrollY >= 20 ? header.classList.add('active') : header.classList.remove('active');
 }
 
 window.addEventListener('scroll', scrollHeader);
@@ -33,5 +33,25 @@ let type = new Typed('.typed', {
 	backSpeed: 100,
 	backDelay: 1000,
 	loop: true
-});
+})
 
+// scroll section active link
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+	const scrolly = window.pageYOffset;
+
+	sections.forEach(section => {
+		const sectionHeight = section.offsetHeight;
+		const sectionTop = section.offsetTop - 50;
+
+		let sectionId = section.getAttribute('id');
+		if(scrolly > sectionTop && scrolly <= sectionTop + sectionHeight){
+			document.querySelector('.menu a[href *='+ sectionId +']').classList.add('active-link');
+		}else{
+			document.querySelector('.menu a[href *='+ sectionId +']').classList.remove('active-link');
+		}
+	})
+}
+
+window.addEventListener('scroll', scrollActive)
